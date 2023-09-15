d7ab1bff41c512b02000e8a19868aaaf460bb7a3
Pranav <pranavlakkadi@gmail.com>
2023-09-15 21:33:42 +0530

    twelveth commit

---
+++ contracts/test/MockV3Aggregator1.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+import "@chainlink/contracts/src/v0.6/tests/MockV3Aggregator.sol";
+
+contract MockV3Aggregator1 is MockV3Aggregator {
+    constructor(uint8 decimal, int256 price) MockV3Aggregator(decimal,price) public {}
+
+  //   function updateRoundData(
+  //   uint80 /*_roundId,*/,
+  //   int256 _answer ,
+  //   uint256 /* _timestamp */,
+  //   uint256 /* _startedAt */
+  // ) public override {
+  //   // latestRound = _roundId;
+  //   latestAnswer = _answer;
+  //   // latestTimestamp = _timestamp;
+  //   getAnswer[latestRound] = _answer;
+  //   // getTimestamp[latestRound] = _timestamp;
+  //   // getStartedAt[latestRound] = _startedAt;
+  // }
+
+}
